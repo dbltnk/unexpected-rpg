df3e882505b73f8f86aee64f4ad5e622e678cc82
Esteba <esteba@gmx.de>
2013-12-08 18:49:41 +0100

    Improved the gamepad handling: Now it does not trigger multiple events anymore if the player holds a button, he has to release it and press it again

--- src/ControllerSupport.java
+++ src/ControllerSupport.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import net.java.games.input.Component;
@@ -18,12 +19,23 @@ public class ControllerSupport extends Thread {
 	/** list of ActionListeners */
 	private List<ActionListener> actionListeners = new ArrayList<ActionListener>();
 	
+	/** map that determines which button is currently pressed on which controller
+	 *  (if there are multiple gamepads, the first one on which the button was
+	 *  pressed "reserves" this button) */
+	private HashMap<String,Controller> buttonPressed;
+	
 	/** 
 	 * constructor: setup all gamepads
 	 */
 	public ControllerSupport() {
-		Controller[] controllerArray = ControllerEnvironment.getDefaultEnvironment().getControllers();
+		// no buttons registered as pressed in the beginning
+		this.buttonPressed = new HashMap<String,Controller>();
+		this.buttonPressed.put("a", null);
+		this.buttonPressed.put("b", null);
+		this.buttonPressed.put("x", null);
+		this.buttonPressed.put("y", null);
 		
+		Controller[] controllerArray = ControllerEnvironment.getDefaultEnvironment().getControllers();
 		this.gamepads = new ArrayList<Controller>();
 	    for (int i =0;i<controllerArray.length;i++) {
 	    	// only consider gamepads
@@ -50,6 +62,34 @@ public class ControllerSupport extends Thread {
 	}
 	
 	/**
+	 * registers a controller that currently presses the given
+	 * button if there is not already another controller that
+	 * has reserved the button (this avoids strange behavior
+	 * if the same button was pressed on multiple controllers)
+	 */
+	public void buttonPressed(Controller newController, String buttonStr) {
+		Controller lastController = this.buttonPressed.get(buttonStr);
+		if (lastController == null) {
+			this.buttonPressed.put(buttonStr,newController);
+			for (ActionListener a : getActionListeners()) {
+        		a.actionPerformed(new ActionEvent(this,0,buttonStr));
+        	}
+		}
+	}
+	
+	/**
+	 * removes the registration of a controller for a given button
+	 * if the button was released on this controller:
+	 * the button can now be pressed again on any controller
+	 */
+	public void buttonReleased(Controller newController, String buttonStr) {
+		Controller lastController = this.buttonPressed.get(buttonStr);
+		if (lastController != null && lastController.equals(newController)) {
+			this.buttonPressed.put(buttonStr, null);
+		}
+	}
+		
+	/**
 	 * run method of the thread:
 	 * supervises the controllers, calls the actionPerformed() method
 	 * of the ActionListeners in case of an event
@@ -57,17 +97,44 @@ public class ControllerSupport extends Thread {
 	@Override
 	public void run() {
 		while(true) {
+			/* check for each gamepad and each of the 4 relevant buttons
+			 * if the button was now pressed or is still pressed on the
+			 * controller */
 	    	for (Controller c : gamepads) {
 	    		c.poll();
 	    		
+	    		if (c.getComponent(Component.Identifier.Button._0).getPollData() > 0) {
+	    			buttonPressed(c,"a");
+	    		} else {
+	    			buttonReleased(c,"a");
+	    		}
+	    		if (c.getComponent(Component.Identifier.Button._1).getPollData() > 0) {
+	    			buttonPressed(c,"b");
+	    		} else {
+	    			buttonReleased(c,"b");
+	    		}
+	    		if (c.getComponent(Component.Identifier.Button._2).getPollData() > 0) {
+	    			buttonPressed(c,"x");
+	    		} else {
+	    			buttonReleased(c,"x");
+	    		}
+	    		if (c.getComponent(Component.Identifier.Button._3).getPollData() > 0) {
+	    			buttonPressed(c,"y");
+	    		} else {
+	    			buttonReleased(c,"y");
+	    		}
+	    		
+	    		/*
 	    		EventQueue queue = c.getEventQueue();
 	    		Event event = new Event();
-	    		
 	    		while (queue.getNextEvent(event)) {
 	                Component comp = event.getComponent();
 	                
 	                String command = "";
 	                if (comp.getIdentifier() == Component.Identifier.Button._0) {
+	                	if (buttonAdown) {
+	                		continue;
+	                	}
 	                	System.out.println("A");  	
 	                	command = "a";
 	                } else if (comp.getIdentifier() == Component.Identifier.Button._1) {
@@ -80,20 +147,27 @@ public class ControllerSupport extends Thread {
 	                	System.out.println("Y");
 	                	command = "y";
 	                } else {
-	                	break;
+	                	continue;
 	                }
 	                
 	                for (ActionListener a : getActionListeners()) {
                 		a.actionPerformed(new ActionEvent(this,0,command));
                 	}
-	             }
+	                
+	                try {
+		                Thread.sleep(500);
+		             } catch (InterruptedException e) {
+		                // TODO Auto-generated catch block
+		                e.printStackTrace();
+		             }
+	             }*/
 	    		
 	    		try {
 	                Thread.sleep(20);
-	             } catch (InterruptedException e) {
+	            } catch (InterruptedException e) {
 	                // TODO Auto-generated catch block
 	                e.printStackTrace();
-	             }
+	            }
 	    	}
 	    }
 	}
